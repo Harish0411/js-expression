be0ade828249b89ffe3907d540e0af3e6e61ac14
Harish0411 <hariharanramasamy98@gmail.com>
2023-02-06 16:25:43 +0530

    adding expression.

---
+++ main.js
@@ -0,0 +1,114 @@
+// addition 
+
+var a=10, b=30;
+
+var c=a+b;
+
+console.log(c);
+
+//subraction
+
+var x=10, y=5;
+
+var z=x-y;
+
+console.log(z);
+
+//multiplication
+
+var m=10, n=20;
+
+var o=m*n;
+
+console.log(o);
+
+//division
+
+var k=25, j=5;
+
+var l=k/j;
+
+console.log(l);
+
+//Area of circle
+
+var p=Math.PI, r=7;
+
+var areaCircle=p*r**2;
+
+console.log(areaCircle);
+
+//Area of triangle
+
+var b=5, h=10;
+
+var areaTriangle=1/2*b*h;
+
+console.log(areaTriangle);
+
+//square
+
+var a=25;
+
+var square=a**2;
+
+console.log(square);
+
+//cube
+
+var b=6;
+
+var cube=b**3;
+
+console.log(cube);
+
+//simple intrest
+
+var p=10000, n=3, r=25;
+
+var SI=p*n*r/100;
+
+console.log(SI);
+
+//Total & average
+
+var n1=75, n2=90, n3=60;
+
+var total=n1+n2+n3;
+
+var avg=total/3;
+
+console.log(total);
+
+console.log(avg);
+
+//larger numer
+
+var p=50, q=25;
+
+console.log(p>q);
+
+//check number positive or not
+
+var n=-6;
+
+console.log(n>0);
+
+//check numer greater than 10
+
+var o=20;
+
+
+console.log(o>10);
+
+//check number even or not
+
+var number=6;
+
+console.log(number%2==0);
+
+//check number is divisible by 5
+
+var d=45;
+
+console.log(d%5==0);
